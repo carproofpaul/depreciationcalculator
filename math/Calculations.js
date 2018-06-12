29babcdc76375ade04c5fbc2626333e0c5bc9647
Laura Mann <LMann@carproof.com>
2018-06-12 15:45:15 -0400

    Returning data properly, not in right order

--- math/Calculations.js
+++ math/Calculations.js
@@ -6,6 +6,7 @@ export const YearsToBuy = (carValues) => {
 
     function findMinSlope(arr) {
         min=arr[0]
+        minIndex=0
         for(k=0;k<arr.length;k++) {
             if(arr[k]<min) {
                 min = arr[k]
@@ -31,17 +32,24 @@ export const YearsToBuy = (carValues) => {
             },
         };
 
-        for(i=0;i<carValues.length-2;i++) {
-            slopes[i] = (carValues[i+2].price-carValues[i].price)/2
+        for(i=carValues.length-1;i>1;i--) {
+            {
+                    slopes.push((carValues[i-2].price - carValues[i].price)/2)
+            }
         }
 
-        bestValues.first.year=carValues[findMinSlope].year
-        bestValues.first.price=carValues[findMinSlope].price
+        for(l=0;l<slopes.length;l++) {
+            console.log(slopes[l])
+        }
+
+        console.log("Min slope: " + findMinSlope(slopes.reverse()))
+        bestValues.first.year=carValues[findMinSlope(slopes)].year
+        bestValues.first.price=carValues[findMinSlope(slopes)].price
 
         slopes.splice(findMinSlope, 1)
 
-        bestValues.second.year=carValues[findMinSlope].year
-        bestValues.second.price=carValues[findMinSlope].price
+        bestValues.second.year=carValues[findMinSlope(slopes)].year
+        bestValues.second.price=carValues[findMinSlope(slopes)].price
 
         
     }
