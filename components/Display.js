a6594f2ecd82148479ecb3be123a83fb4ec2e37f
Laura Mann <LMann@carproof.com>
2018-06-13 15:45:23 -0400

    Added cards for best options

--- components/Display.js
+++ components/Display.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { StyleSheet, Text, View, TouchableOpacity, Alert, ScrollView } from 'react-native';
 import Icon from 'react-native-vector-icons/FontAwesome';
-import { ListItem } from 'react-native-elements';
+import { ListItem, Card, Header } from 'react-native-elements';
 
 const WIDTH = 300
 
@@ -40,6 +40,28 @@ export default class Display extends React.Component {
                 name='info' />
         </View>
         <View style={styles.textContainer}>
+
+        <View style={{flex: 1, flexDirection: 'column'}}>
+        <Card titleStyle={styles.cardHeader} title="BEST VALUE">
+        <View>
+          <Text style={{textAlign: 'center', fontSize:30, fontWeight: 'bold'}}>{this.props.analysedData[0].year}</Text>
+          <Text style={{fontSize: 16, textAlign: 'center'}}>{this.props.info.make} {this.props.info.model}</Text>
+          <Text style={{fontSize: 14, textAlign: 'center'}}><Icon name="shopping-cart"/> Buy now for ${this.props.analysedData[0].price}</Text>
+          <Text style={{fontSize: 14, textAlign: 'center'}}><Icon name="arrow-down"/> Value will depreciate {this.props.analysedData[0].percentage.toFixed(2)}% after 2 years</Text>
+        </View>
+        </Card>
+
+         <Card titleStyle={styles.cardHeader} title="RUNNER UP">
+        <View>
+          <Text style={{textAlign: 'center', fontSize:25, fontWeight: 'bold'}}>{this.props.analysedData[1].year}</Text>
+          <View style={{flexDirection: 'row', justifyContent: 'space-between'}}>
+            <Text><Icon name="shopping-cart"/> ${this.props.analysedData[1].price}</Text>
+            <Text><Icon name="arrow-down"/> {this.props.analysedData[1].percentage.toFixed(2)}%</Text>
+          </View>
+        </View>
+        </Card>
+        </View>
+        
           <Text style={{fontSize: 20, margin: 10, textAlign: 'center', fontWeight: 'bold'}}>
               To minimize the impact of depreciation, we recommend buying a {this.props.analysedData[0].year} or a {this.props.analysedData[1].year} {this.props.info.make} {this.props.info.model}
           </Text>
@@ -93,4 +115,14 @@ const styles = StyleSheet.create({
     color: 'white',
     fontWeight: '700',
   },
+  cards : {
+    flexDirection: 'column',
+    justifyContent: 'space-between'
+  },
+  cardHeader : {
+    textAlign: 'center',
+    fontSize: 20,
+    fontWeight: 'bold',
+    color: '#3993EE'
+  }
 });
