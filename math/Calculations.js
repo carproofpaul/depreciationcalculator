50feeffa2c0cc7d81fc8ce24ffb2bfb69ca8e8e8
Laura Mann <LMann@carproof.com>
2018-06-14 10:14:36 -0400

    Fixed error of adding undefined into percentages array

--- math/Calculations.js
+++ math/Calculations.js
@@ -7,20 +7,25 @@ export const YearsToBuy = (carValues, length) => {
         //create percentages array and return object
         percentages = []
         bestValues = []
-
+        
         //Loop through given array and add years, prices, and calculated percentages to new array
         for(i=0;i<carValues.length-length;i++) {
             sum = parseInt(i)+parseInt(length)
             //if current car price or car 2 years olders is 0 don't include
-            if(carValues[i].price!=0 && carValues[sum].price!=0 && sum < carValues.length) {
-                percentages[i] = {
-                year : carValues[i].year,
-                price : carValues[i].price,
-                percentage: 100-(carValues[sum].price/carValues[i].price)*100,
-            }
+            if(sum < carValues.length) {
+                // console.log("Sum less than length:" + sum)
+                if(carValues[i].price>0 && carValues[sum].price>0) {
+                    percent = 100-(carValues[sum].price/carValues[i].price)*100
+                    percentages[i] = {
+                    year : carValues[i].year,
+                    price : carValues[i].price,
+                    percentage: 100-(carValues[sum].price/carValues[i].price)*100,
+                    }
+                }
 
             }
-            else if(carValues[i].year==2001) {
+            else if(carValues[i].year==2001 && carValues[i].price>0) {
+                console.log("Year 2009 found")
                 percentages[i] = {
                     year : carValues[i].year,
                     price : carValues[i].price,
@@ -28,9 +33,11 @@ export const YearsToBuy = (carValues, length) => {
                 }
             }
         }
-
-        //sort array by percentage and set return object to years with the lowest percentages
+        //sort array by percentage & filter out undefined values
         percentages = percentages.sort(function(a, b){return a.percentage - b.percentage})
+        percentages= percentages.filter(function( element ) {
+            return element !== undefined;
+         });
 
 
     return (
